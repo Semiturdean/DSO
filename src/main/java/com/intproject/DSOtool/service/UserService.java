7e815ae69f16a6557397747be6d72afff4b955dd
Semiturdean <Semiturdean@hotmail.com>
2018-10-19 13:43:19 +0200

    Added RoleRepo to UserService to try and mapp Role and User properly

--- src/main/java/com/intproject/DSOtool/service/UserService.java
+++ src/main/java/com/intproject/DSOtool/service/UserService.java
@@ -2,6 +2,7 @@ package com.intproject.DSOtool.service;
 
 
 import com.intproject.DSOtool.data.User;
+import com.intproject.DSOtool.repositories.RoleRepository;
 import com.intproject.DSOtool.repositories.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import com.intproject.DSOtool.service.exceptions.UserExceptions;
@@ -13,10 +14,12 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     public User createNewUserAccount(User userDto){
@@ -50,7 +53,7 @@ public class UserService {
     public User findUserByUsername(String username){
         return userRepository.findByUsername(username).get();
     }
-    
+
     private void validateUser(User user) {
         if (user.getUsername().length() < 10) {
             throw new UserExceptions("Username cannot be shorter than 10 characters");
